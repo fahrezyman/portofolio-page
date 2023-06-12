beeb0d3e66c5682fd67a37f35f9c87d5dbaf68ac
fahrezyman <ilhamfahrezyxz@gmail.com>
2023-06-12 07:11:41 +0700

    Adding Merch Section with DOM

--- js/script.js
+++ js/script.js
@@ -13,3 +13,31 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+//DOM Fetch Product Merch
+
+const productContainer = document.querySelector(".row-product ul");
+
+fetch("/json/merch-product.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data)
+
+    let contentHTML = "";
+    data.products.forEach((product) => {
+      contentHTML += `
+      <div class="card">
+      <img src="${product.image}" alt="${product.name}" style="width: 100%" />
+      <h1>${product.name}</h1>
+      <p class="price">${product.price}</p>
+      <p>${product.description}</p>
+      <p><button>Add to Cart</button></p>
+      </div>
+      `;
+    });
+    // console.log(isiHTML)
+    productContainer.innerHTML = contentHTML;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
